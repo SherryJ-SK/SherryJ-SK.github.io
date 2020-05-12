7f14b0b2e9fe5ddd1f31601d6b21be7f873bfb28
Sherry <a-sherry@live.com>
2020-05-13 00:55:16 +1000

    recreate gitignore

---
+++ build/precache-manifest.5af2ff7e5ddedd49b84f491417780904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d74b3d737638def8d394685ebe153aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfe36d17385c5604973f",
+    "url": "/static/js/2.77b445c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77b445c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b4b038323f189515fd",
+    "url": "/static/js/main.55e26136.chunk.js"
+  },
+  {
+    "revision": "33bd096a9729b8884e50",
+    "url": "/static/js/runtime-main.e5745624.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a39f7370259218675db9d0b02ab26fb0.js"
+  "/precache-manifest.5af2ff7e5ddedd49b84f491417780904.js"
 );
 
 self.addEventListener('message', (event) => {
